a189136023739c5fd8b52b8588d273065e4d657c
pherbelin <Pierre.HERBELIN@consulting-for.accor.com>
2021-11-10 13:18:43 +0100

    Test some fancy features

---
+++ test.js
@@ -0,0 +1,30 @@
+
+let currentPopup;
+
+WA.state.saveVariable('testUrl', 'test.html').catch(e => console.error('Something went wrong while saving variable', e));
+WA.state.onVariableChange('testUrl').subscribe((value) => {
+    console.log('Variable "testUrl" changed. New value: ', value);
+});
+
+WA.room.onEnterZone('testZone', () => {
+  currentPopup = WA.ui.openPopup("popupRectangle", 'Hello world!', [{
+         label: "Close",
+         className: "secondary",
+         callback: (popup) => {
+             popup.close();
+         }
+     }]);
+})
+
+WA.room.onLeaveZone('testZone', () => {
+  WA.state.saveVariable('testUrl', 'test.html').catch(e => console.error('Something went wrong while saving variable', e));
+  closePopUp();
+});
+
+
+function closePopUp(){
+    if (currentPopup !== undefined) {
+        currentPopup.close();
+        currentPopup = undefined;
+    }
+}
