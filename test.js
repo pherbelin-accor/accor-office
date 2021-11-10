90022f09151fdf2be4227aa82874e93585285a3a
pherbelin <Pierre.HERBELIN@consulting-for.accor.com>
2021-11-10 13:27:42 +0100

    Fix floor

--- test.js
+++ test.js
@@ -1,11 +1,10 @@
 
 let currentPopup;
 
-WA.state.saveVariable('testUrl', 'test.html').catch(e => console.error('Something went wrong while saving variable', e));
 WA.state.onVariableChange('testUrl').subscribe((value) => {
     console.log('Variable "testUrl" changed. New value: ', value);
 });
-
+WA.state.saveVariable('testUrl', 'test.html').catch(e => console.error('Something went wrong while saving variable', e));
 WA.room.onEnterZone('testZone', () => {
   currentPopup = WA.ui.openPopup("popupRectangle", 'Hello world!', [{
          label: "Close",
