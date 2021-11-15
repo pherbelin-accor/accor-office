6e9161be1d84ad00fc13dfb2b21a8fb9d57ce132
pherbelin-accor <91456436+pherbelin-accor@users.noreply.github.com>
2021-11-15 19:49:02 +0000

    Deploying to gh-pages from master @ 221419bb1d9c4703287cbab6a24e00c5be088911 🚀

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,35 @@ import {bootstrapExtra} from "@workadventure/scripting-api-extra";
 bootstrapExtra().catch(e => console.error(e));
 
 let currentPopup: any = undefined;
-const today = new Date();
-const time = today.getHours() + ":" + today.getMinutes();
 
-WA.room.onEnterZone('clock', () => {
-    currentPopup =  WA.ui.openPopup("clockPopup","It's " + time,[]);
+WA.state.onVariableChange('freeze players').subscribe((value: boolean) => {
+  if (value == true && WA.player.name == 'Pierre'){
+    WA.controls.disablePlayerControls();
+  } else {
+    WA.controls.restorePlayerControls();
+  }
+});
+
+WA.room.onEnterZone('testZone', () => {
+  console.log(WA.state.loadVariable('enable second floor'));
+
+  if (WA.state.loadVariable('enable second floor')){
+    currentPopup = WA.ui.openPopup("popup_elevator", "Voulez-vous changer d'étage?", [{
+           label: "Oui",
+           className: "primary",
+           callback: (popup) => {
+             WA.nav.goToRoom('cafet.json');
+               popup.close();
+           }
+       }]);
+  }
 })
 
-WA.room.onLeaveZone('clock', closePopUp)
+
+WA.room.onLeaveZone('testZone', () => {
+  closePopUp();
+});
+
 
 function closePopUp(){
     if (currentPopup !== undefined) {
